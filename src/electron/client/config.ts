949ad5099cf9be1290c3907324d11fb1f3aabae3
sadcatjam <73041506+sadcatjam@users.noreply.github.com>
2021-12-28 11:32:11 +0500

    chore: 2.0.1

--- src/electron/client/config.ts
+++ src/electron/client/config.ts
@@ -49,7 +49,7 @@ export default function readConfig(): Config {
         integration: '',
         pool: 'wss://pplns.toncoinpool.io/stratum',
         rig: 'default',
-        version: '2.0.0',
+        version: '2.0.1',
         wallet: '',
         ...jsonConfig,
         ...cliConfig
